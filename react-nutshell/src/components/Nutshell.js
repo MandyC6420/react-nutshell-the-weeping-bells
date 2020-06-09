0d78ac33e7562700f24cdf30c7d6ae456733ff6f
Mandy <drgnfli6420@yahoo.com>
2020-06-09 11:48:05 -0400

    chat card and nutshell cards complete

---
+++ react-nutshell/src/components/Nutshell.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import './Nutshell.css'
+
+class Nutshell extends Component {
+    render() {
+        return (
+	<div class="container-main">
+	    	<div class="section-content">
+                	<h2>Student Kennels<br />
+                   	 	<small>Loving care when you're not there.</small>
+                	</h2>
+                	<address>
+                    		Visit Us at the Nashville North Location
+                    		<br />500 Puppy Way
+                	</address>
+            	</div>
+	</div>
+	
+        );
+    }
+}
+
+export default Nutshell
