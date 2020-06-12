c02fa63b584e2df21a1115bc27b687be640c62a9
StephenAvila2020 <stephen.r.avila@gmail.com>
2020-06-12 11:52:01 -0400

    added

--- react-nutshell/src/components/events/EventCard.js
+++ react-nutshell/src/components/events/EventCard.js
@@ -7,17 +7,22 @@ class EventCard extends Component {
     return (
       <div className="card">
         <div className="card-content">
-          
+       
           <h3>Name: <span className="card-eventname">{this.props.event.name}</span></h3>
           <p>Date: {this.props.event.date}</p>
           <p>Location: {this.props.event.location}</p>
           <Link to={`/events/${this.props.event.id}`}><button>Event Details</button></Link>
           <button type="button"
-        onClick={() => {this.props.history.push(`/events/${this.props.event.id}/edit`)}}>Edit Event</button>
+            onClick={() => { this.props.history.push(`/events/${this.props.event.id}/edit`) }}>Edit Event</button>
         </div>
       </div>
+
     );
+    
   }
+  
 }
+
+
 
 export default EventCard;
--- react-nutshell/src/components/home/Home.js
+++ react-nutshell/src/components/home/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+
 class Home extends Component {
   render() {
     return (
@@ -9,6 +10,7 @@ class Home extends Component {
       </address>
     )
   }
+  
 }
 
 export default Home
